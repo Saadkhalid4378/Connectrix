eeecf396ec4e6aad586b2dd87001a7e4043bff76
saniaanmol <saniaanmol37@gmail.com>
2024-01-12 15:39:50 +0500

    thought views and urls created

---
+++ thought/views.py
@@ -0,0 +1,27 @@
+ 
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Thought
+from .forms import ThoughtForm  
+
+# Create your views here.
+
+
+@login_required
+def thought_detail(request, thought_id):
+    thought = get_object_or_404(Thought, pk=thought_id)
+    return render(request, 'thought_detail.html', {'thought': thought})
+
+# @login_required
+def create_thought(request):
+    if request.method == 'POST':
+        form = ThoughtForm(request.POST, request.FILES)
+        if form.is_valid():
+            thought = form.save(commit=False)
+            thought.user = request.user
+            thought.save()
+            return redirect('thought_detail', thought_id=thought.id)
+    else:
+        form = ThoughtForm()
+    
+    return render(request, 'create_thought.html', {'form': form})

a50d2ce002bfb4f8ad383ace6d3d45a8d6b96431
Saad Ali <saadali4378@gmail.com>
2024-01-13 20:33:52 +0500

    bug fix at admin.py in user and thought

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from user.models import User
+from user.models import User, Profile
 # Register your models here.
 
 
 admin.site.register(User)
+admin.site.register(Profile)

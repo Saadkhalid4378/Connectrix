d93d9b7225680b3e8e8632e4a05d6fa182356636
saniaanmol <saniaanmol37@gmail.com>
2024-01-16 17:56:01 +0500

    thought like vies and url created

--- thought/views.py
+++ thought/views.py
@@ -1,13 +1,43 @@
  
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Thought
+from .models import Thought, Like
 from .forms import ThoughtForm  
 from django.views.generic import CreateView, ListView
 from django.views.generic.detail import DetailView
 from user.models import User
 
 # Create your views here.
+
+@login_required
+def like_thought(request, thought_id):
+    thought = get_object_or_404(Thought, pk=thought_id)
+    likes = Like.objects.filter(thought=thought)
+    is_liked = likes.filter(user=request.user).exists()
+
+    if request.method == 'POST':
+        if 'like_button' in request.POST:
+            if not is_liked:
+                like = Like(user=request.user, thought=thought)
+                like.save()
+            else:
+                # Unlike if already liked
+                likes.filter(user=request.user).delete()
+
+            # Redirect to the same page after handling the like action
+            return redirect('thought_detail', thought_id=thought.id)
+
+    context = {
+        'thought': thought,
+        'likes': likes,
+        'is_liked': is_liked,
+    }
+
+    return render(request, 'thought_detail.html', context)
+
+
+
+
 
 
 class User_thoughts(ListView):
@@ -40,3 +70,18 @@ def create_thought(request):
         form = ThoughtForm()
     
     return render(request, 'thought.html', {'form': form})
+
+
+
+
+
+
+
+# def like_thought(request):
+#     thought = thought.objects.all()
+#     likes = Like.objects.all()
+#     context = {
+#        'thought': thought,
+#        'likes': likes,
+#     }
+#     return render(request, 'like_thought.html', context)

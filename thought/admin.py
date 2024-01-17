8b8bc4d7afafed81f75390daaf7661329bb9b795
saniaanmol <saniaanmol37@gmail.com>
2024-01-17 13:50:16 +0500

    bug fix in thought like html fle

--- thought/admin.py
+++ thought/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from thought.models import Thought
 # Register your models here.
 
-
-admin.site.register(Thought)
+class Thought_detail(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'title']
+admin.site.register(Thought, Thought_detail)
